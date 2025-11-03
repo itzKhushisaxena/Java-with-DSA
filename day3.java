effc62ae84dd7bbfac1fd3752319fdd319226b27
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-11-03 20:00:07 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+//import java.util.Scanner;
+public class day3 {
+    public static void main(String[] args) {
+//        int num1 = 1;
+//        int num2 = 2;
+//        int num3 = 7;
+//        float num5 = 7.67f;
+//        System.out.println(num5);
+//        int suman = (int) num5;
+//        System.out.println(suman);
+//        System.out.println((float) (num1 + num2 + num3) / 3);
+//        int sq=6;
+//        System.out.println(sq*sq);
+//        System.out.println(Math.sqrt(64));
+//        System.out.println(Math.cbrt(64));
+//        System.out.println(Math.min(6,4));
+//        System.out.println(Math.max(6,4));
+//        System.out.println(Math.ceil(6.74));
+//        System.out.println(Math.floor(6.784));
+//        System.out.println(Math.round(6.384));
+//        System.out.println(Math.abs(6.674));
+//        System.out.println(Math.log10(100));
+//        int num1=8;
+//        int num2=9;
+//        System.out.println((double)(num1+num2)/2);
+//        if(){
+//
+//        } else {
+//
+//        }
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("enter your age");
+//        int age = sc.nextInt();
+//        if (age < 0 || age > 120) {
+//            System.out.println("invalid age");
+//        } else if (age >= 0 && age <= 18) {
+//            System.out.println("you can't drive the car");
+//        } else if (age > 18 && age <= 80) {
+//            System.out.println(" you can drive the car");
+//        } else {
+//            System.out.println("you are over aged");
+        }
+    }
+
+
+
+
+
+
