473c58d9c6f4cb2c2e4bb40b75d1475c544e2a0c
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-11-04 19:38:26 +0530

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class day4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number");
+//        int num=sc.nextInt();
+//        for(int i=0;i<2*num;i++){
+//            if(i%2==0)
+//         System.out.println(i);
+        int num = sc.nextInt();
+
+//        switch(day){
+//            case 1:
+//                System.out.println("Today is sunday");
+//                break;
+//            case 2:
+//                System.out.println("Today is monday");
+//                break;
+//            case 3:
+//                System.out.println("Today is Tuesday");
+//                break;
+//            case 4:
+//                System.out.println("Today is wednesday");
+
+        }
+
+}
+
+
+
