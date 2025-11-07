5c263a8f8b3a4f96a094807048b3abdb09682ae1
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-11-07 22:42:30 +0530

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class day6 {
+    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter the no. of row");
+//        int n=sc.nextInt();
+//        for(int i=1;i<n;i++){
+//         for(int j=0;j <n-1;j++){
+//             System.out.print(" ");
+//            }
+//             System.out.print("1 ");
+//         }
+//            System.out.println("0");
+//        int[] arr;
+//arr=new int[20];
+//Scanner sc=new Scanner(System.in);
+//for(int i=0;i<5;i++){
+//    System.out.println("enter the value at index"+i);
+//    arr[i]=sc.nextInt();
+//}
+//for(int i=0;i<5;i++){
+//    System.out.println("the value at index"+i+"is "+arr[i]);
+}
+        }
+
+
