076a732b5c3f84e2d6d9844ce332760ae3b0863e
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-11-07 22:43:01 +0530

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day7 {
+    public static void main(String[] args) {
+//        float result=5*4+8-4/2;
+//        System.out.println(result);
+//        int[] arr=new int[20];
+//        arr[0]=7;
+//        arr[1]=5;
+//        arr[2]=6;
+//        arr[3]=8;
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[3]);
+        }
+    }
