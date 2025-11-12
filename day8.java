b7d18e660fb4d7437a8d406895091e8ad157ca11
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-11-12 09:52:41 +0530

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the row");
+        int n = sc.nextInt();
+//        for(int i=1;i<6;i++) {
+//            for (int j = 1; j <=i ; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        for(int i=0;i<=5;i++){
+//            for(int j=0;j<=n-i ;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//      }
+    }
+}
