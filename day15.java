5d898e82f8b90617666c2671d4f71c25f3f3cb8b
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-11-17 09:04:38 +0530

    Add files via upload

---
+++ day15.java
@@ -0,0 +1,14 @@
+
+
+public class day15 {
+    static void addtwonum(int a,int b) {
+        System.out.println(a * b);
+        return;
+    }
+
+    public static void main(String[] args) {
+        addtwonum(4,5);
+        System.out.println();
+
+    }
+}
