202c4cd9d3b86e7be0d7b0d6c94c9fd1a48037f7
Khushi Saxena <khushisaxenaa28@gmail.com>
2025-12-05 09:42:29 +0530

    Add files via upload

---
+++ day31.java
@@ -0,0 +1,18 @@
+import java.lang.classfile.ClassModel;
+
+public class Program14 {
+    public static int findLCM(int num1,int num2){
+        int max=(num1>num2)?num1:num2;
+                while(true){
+                    if(max%num1==0&&max%num2==0){
+                        return max;
+
+
+                    }else;
+                    max++;
+                }
+    public static void main(String[] args) {
+        num1=
+        System.out.println(findLCM( num1, num2));
+    }
+}
