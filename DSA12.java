6d81488b01e0c2e3b45bf94c335da65f04f8c0ff
Khushi Saxena <itzkhushisaxena1@gmail.com>
2026-02-14 12:41:39 +0530

    Add files via upload

---
+++ DSA12.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class DSA12 {
+
+   public static void main(String[] args) {
+Stack s1=new Stack();
+s1.add(11);
+s1.add(22);
+s1.add(33);
+s1.add(44);
+       System.out.println(s1);
+       System.out.println(s1.peek());
+       System.out.println(s1);
+       System.out.println(s1.size());
+       System.out.println(s1.pop());
+       System.out.println(s1);
+       System.out.println(s1.empty());
+    }
+}
